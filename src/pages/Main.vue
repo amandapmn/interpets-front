dd910ffecf9c29be51877c82cc1937bd4e6b9ce2
igorlmgx <igorlmgx@gmail.com>
2020-08-17 19:20:36 -0300

    Texto na página inicial

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,6 +1,13 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center text-center">
     <img alt="Quasar logo" src="~assets/logo.png">
+    <p>
+      O Pré-Sudeste possui o intuito de deliberar discussões, pautas para serem levadas ao Sudeste PET 2020, como uma opinião do PET UFSCar representando todos os PETs da UFSCar.
+      Este ano o evento contará com uma Abertura e os GDT's (Grupos de Discussão e Trabalho) na parte da manhã e a Assembleia na parte da tarde.
+      Neste site encontram-se as descrições e os links para a inscrição no GDT de seu interesse.
+      <br>As inscrições para o Pré-Sudeste 2020 estarão abertas do dia 28/08 ao dia 11/09.
+      <br>Esperamos por vocês!
+    </p>
   </q-page>
 </template>
 
