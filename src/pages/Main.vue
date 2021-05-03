12efdaa68a30fe300c4022e95a3c0130bbd4f675
amandapmn <40667726+amandapmn@users.noreply.github.com>
2021-05-03 14:48:56 -0300

    Mudando texto 2021

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -2,10 +2,10 @@
   <q-page class="text-center">
     <img alt="Quasar logo" id="logo" src="~assets/logo.png" />
     <p>
-      O Pré-Sudeste possui o intuito de deliberar discussões, pautas para serem levadas ao Sudeste PET 2020, como uma opinião do PET UFSCar representando todos os PETs da UFSCar.
-      Este ano o evento contará com uma Abertura e os GDT's (Grupos de Discussão e Trabalho) na parte da manhã e a Assembleia na parte da tarde.
-      Neste site encontram-se as descrições e os links para a inscrição no GDT de seu interesse.
-      <br />As inscrições para o Pré-Sudeste 2020 estarão abertas do dia 28/08 ao dia 11/09.
+      O Pré-Sudeste possui o intuito de deliberar discussões e pautas para serem levadas ao Sudeste PET 2021, como uma opinião do PET UFSCar representando todos os PETs da UFSCar.
+      Este ano o evento contará com a Abertura e os GDT's (Grupos de Discussão e Trabalho) na parte da manhã (8:00 às 12:00) e a Assembleia na parte da tarde (14:00 às 17:00).
+      Neste site, encontram-se as descrições e os links para a inscrição no GDT de seu interesse.
+      <br />As inscrições para o Pré-Sudeste 2021 estarão abertas do dia 07/05 ao dia 14/05.
       <br />Esperamos por vocês!
     </p>
   </q-page>
